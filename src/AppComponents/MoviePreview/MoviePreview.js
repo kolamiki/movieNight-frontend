112dd029c927394f856af606cbe72f2679e1bdb0
kolamiki <kolamiki@gmail.com>
2025-03-24 22:33:14 +0100

    Add MoviePreview component with initial structure

---
+++ src/AppComponents/MoviePreview/MoviePreview.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function MoviePreview({
+  apiOrigin,
+  movieId,
+  isPreviewActive,
+  setIsPreviewActive,
+}) {
+  //Na ten moment komponent działa tylko po przyjęciu movieID.
+  // Sprawdź, czy film o wpisanym ID występuje w bazie danych, w przeciwnym wypadku pobierz dane z imdb przy pomocy viewMovieDetails
+
+  return <></>;
+}
+
+export default MoviePreview;
